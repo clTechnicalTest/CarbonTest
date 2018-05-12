6a893f65cb22ed283f3d3e956d509a680c89a66d
clement <clement@rickert.home>
2018-05-12 15:18:05 +0200

    first commit

---
+++ Sources/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Next();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
